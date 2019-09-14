017b862d88cbef884a4638823f6aa59b4482fc59
kamec <undinakamec@gmail.com>
2019-09-14 19:56:15 +0300

    Updated versions to latest, Eslint added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react' // точки с запятыми как вариант конечно
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import thunk  from 'redux-thunk'
@@ -8,10 +8,11 @@ import App from './containers/App'
 import reducer from './reducers/reducer'
 import './index.css'
 
-let store = createStore(reducer, applyMiddleware(thunk));
+let store = createStore(reducer, applyMiddleware(thunk)); // why let?
 
+// делает стор доступным дочерним компонентам
 ReactDOM.render(
-  <Provider store={store}> // делает стор доступным дочерним компонентам
+  <Provider store={store}> 
     <App/>
   </Provider >,
   document.getElementById('root'));
